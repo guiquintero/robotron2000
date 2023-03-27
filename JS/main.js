2b8fccdb5eac56ab49a57d577d9b72d9d7a413ac
Quintero <guiquintero02@gmail.com>
2023-03-27 11:23:51 -0300

    Adicionando projeto novo

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+const robotron = document.querySelector("#robotron");
+
+robotron.addEventListener("click", dizOi);
+
+function dizOi(){
+    console.log("oi");
+}
+
+/*          ALTERNATIVA
+
+robotron.addEventListener("click", function() {
+    console.log("oi");
+})
+
+robotron.addEventListener("click", () => {
+    console.log("oi");
+})
+
+*/
+
+
+
